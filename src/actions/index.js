6f30a84d3fa783dcb3fc8c29f0ed494e3027b350
ergot <wjose14@hotmail.com>
2017-10-26 15:30:38 +0200

    5-55

---
+++ src/actions/index.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+import {AT_POSTS} from "./action-types";
+const END_POINT = 'http://localhost:300'
+
+export function readAllPost() {
+    return function (dispatch) {
+        axios.get(`${END_POINT}/posts`).then((response)=>{
+            dispatch({type: AT_POSTS.READ_ALL_POST, payload: response.data})
+        })
+    }
+}
+
+export function readPost(id) {
+    return function (dispatch) {
+        axios.get(`${END_POINT}/posts/${id}`).then((response)=>{
+            dispatch({type: AT_POSTS.READ, payload: response.data})
+        })
+    }
+}
+
+export function deletePost() {
+    return function (dispatch) {
+        axios.delete(`${END_POINT}/posts/${id}`).then((response)=>{
+            dispatch({type: AT_POSTS.DELETE, payload: id})
+        })
+    }
+}
+
+export function createPost(post) {
+    return function (dispatch) {
+        axios.post(`${END_POINT}/posts`, {title:post.title, content: post.content, author: post.author}).then((response)=>{
+            dispatch({type: AT_POSTS.CREATE, payload: response.data})
+        })
+    }
+}
