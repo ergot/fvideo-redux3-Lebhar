d25ee4e61ba6707bc7578efd5e6e2849ca454051
ergot <wjose14@hotmail.com>
2017-10-27 17:11:43 +0200

    5-65

--- src/containers/post-form.js
+++ src/containers/post-form.js
@@ -1,25 +1,36 @@
 import React from 'react'
 import {Link} from 'react-router'
+import {reduxForm} from 'redux-form'
+import {createPost} from '../actions/index'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+
+const formConfig = {
+    form: 'createPostForm',
+    fields: ['title', 'content', 'author']
+}
 
 class PostForm extends React.Component {
     render() {
+        const {fields} = this.props
+
         return (
             <div>
                 <h1>Nouveau post</h1>
                 <form action="">
                     <div className="form-group">
                         <label >titre</label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" className="form-control" {...fields.title}/>
                         <div></div>
                     </div>
                     <div className="form-group">
                         <label >descirption</label>
-                        <input type="textarea" className="form-control"/>
+                        <input type="textarea" className="form-control" {...fields.content}/>
                         <div></div>
                     </div>
                     <div className="form-group">
                         <label >Auteur</label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" className="form-control" {...fields.author}/>
                         <div></div>
                     </div>
                     <Link to={'/'} className='button_space'><button className='btn btn-danger'>Retour</button></Link>
@@ -30,4 +41,4 @@ class PostForm extends React.Component {
     }
 }
 
-export default PostForm
+export default connect(null,null)(reduxForm(formConfig)(PostForm))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import ReducerPosts from './reducer-posts'
 import ReducerActivePost from './reducer-active-post'
+import {reducer as ReducerForm } from 'redux-form'
+
 
 const rootReducer = combineReducers({
   posts: ReducerPosts,
-    activePost: ReducerActivePost
+    activePost: ReducerActivePost,
+    form: ReducerForm
 });
 
 export default rootReducer;
