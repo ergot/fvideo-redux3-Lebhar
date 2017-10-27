f96c1b17d9fc59c8203f3a3a25182ce80760d1b9
ergot <wjose14@hotmail.com>
2017-10-27 12:51:04 +0200

    5-59

--- src/containers/post.js
+++ src/containers/post.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import PostContent from '../components/post-content'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {readPost} from "../actions/index";
+
 
 class Post extends React.Component {
+    componentWillMount () {
+        this.props.readPost (this.props.params.id)
+    }
+
+    renderPostContent(){
+        const {post} = this.props
+        if(post) {
+            return <PostContent post={post}/>
+        }
+    }
+
     render() {
+        console.log(this.props.post)
+
         return (
             <div>
-                post num: {this.props.params.id}
-                <PostContent/>
+                {this.renderPostContent()}
             </div>
         )
     }
 }
 
-export default Post
+const mapStateToProps =(state) => {
+    return {
+        post: state.activePost
+    }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    ...bindActionCreators({readPost}, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
