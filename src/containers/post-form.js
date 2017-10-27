ce9af9d47ab07fd717a3c4e09ce32c3c72b9d2d6
ergot <wjose14@hotmail.com>
2017-10-27 16:31:31 +0200

    5-63

--- src/containers/post-form.js
+++ src/containers/post-form.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import {Link} from 'react-router'
 
 class PostForm extends React.Component {
     render() {
         return (
             <div>
                 <h1>Nouveau post</h1>
+                <form action="">
+                    <div className="form-group">
+                        <label >titre</label>
+                        <input type="text" className="form-control"/>
+                        <div></div>
+                    </div>
+                    <div className="form-group">
+                        <label >descirption</label>
+                        <input type="textarea" className="form-control"/>
+                        <div></div>
+                    </div>
+                    <div className="form-group">
+                        <label >Auteur</label>
+                        <input type="text" className="form-control"/>
+                        <div></div>
+                    </div>
+                    <Link to={'/'} className='button_space'><button className='btn btn-danger'>Retour</button></Link>
+                    <button className="btn btn-primary" type='submit'>cree</button>
+                </form>
             </div>
         )
     }
--- src/containers/post-list.js
+++ src/containers/post-list.js
@@ -4,6 +4,7 @@ import {bindActionCreators} from 'redux'
 import {readAllPost, deletePost} from "../actions/index";
 import PostListItem from '../components/post-list-item'
 import ReactCSSTransitionGroup from 'react-transition-group/CSSTransitionGroup'
+import {Link} from 'react-router'
 
 class PostList extends React.Component {
     componentWillMount (){
@@ -27,6 +28,11 @@ class PostList extends React.Component {
         return (
             <div>
                 <h1>Liste des postes</h1>
+                <div className="button_add">
+                    <Link to={'create-post'}>
+                        <button className="btn btn-primary btn-circle btn-lg">+</button>
+                    </Link>
+                </div>
                 <table className="table table-hover">
                     <thead>
                     <tr>
